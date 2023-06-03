6756f423665c65dab7a19645e792674468e2e049
BernardooDev <bernardo.almeida@uscsonline.com.br>
2023-06-02 22:05:51 -0300

    Update the project

---
+++ home.js
@@ -0,0 +1,61 @@
+history.replaceState(null, '', './home.html');
+// Limpar os parâmetros de email e senha na URL.
+
+document.addEventListener("DOMContentLoaded", () => {
+const imageStatic = document.getElementById("imageStatic");
+const imageProfile = document.querySelector("#imageProfile");
+const selectFile = document.getElementById("selectFile");
+
+imageProfile.addEventListener("change", function () {
+    const imgProfile = new FileReader();
+
+    imgProfile.addEventListener("load", () => {
+        imageStatic.setAttribute("src", imgProfile);
+        localStorage.setItem("imgProfile", imgProfile.result);
+        window.location.reload()
+    });
+
+    imgProfile.readAsDataURL(this.files[0]);
+})
+
+const newImgProfile = localStorage.getItem("imgProfile");
+    if(newImgProfile) {
+        imageStatic.setAttribute("src", newImgProfile);
+    }  
+});
+
+const infoProfile = document.querySelector(".infoProfile")
+const userName = localStorage.getItem("name");
+const userEmail = localStorage.getItem("email");
+const userPhone = localStorage.getItem("cellphone")
+
+const divName = document.createElement("div");
+divName.setAttribute("id","infoDiv");
+divName.innerHTML =
+`
+    <label for="userEmail">Nome:</label>
+    <p id="userName">${userName}</p>
+`;
+infoProfile.appendChild(divName);
+
+const divEmail = document.createElement("div");
+divEmail.setAttribute("id","infoDiv");
+divEmail.innerHTML =
+`
+    <label for="userEmail">Email:</label>
+    <p id="userEmail">${userEmail}</p>
+`;
+infoProfile.appendChild(divEmail);
+
+const divPhone = document.createElement("div");
+divPhone.setAttribute("id","infoDiv");
+divPhone.innerHTML =
+`
+    <label for="userEmail">Celular:</label>
+    <p id="userEmail">${userPhone}</p>
+`;
+infoProfile.appendChild(divPhone);
+
+
+
+
---
+++ login.js
@@ -0,0 +1,26 @@
+const formLogin = document.getElementsByClassName("formLogin")
+// Pega o formulário 
+
+formLogin.addEventListener("submit", function() {
+    e.preventDefault();
+// Previne o formulário de ficar recarregando a página
+
+    var storedEmail = localStorage.getItem('email');
+    var storedPassword = localStorage.getItem('password');
+// Armazenda os dados que foram colocados anteriormentes em uma váriavel 
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+// Pega os valores de cada input
+
+    if (email === storedEmail && password === storedPassword) {
+        alert('Login bem-sucedido!');
+        this.submit();
+
+// Verifica se os valores de cada campo são coincidente ao dados armazenados 
+
+    } else {
+        alert('Email ou senha inválidos. Tente novamente!');
+    }
+// Caso não sejam coincidente, informa o usuário.
+});
